da9f08827dba95f8b96751e66c8ee42d897eeb37
thuanbnv-1024 <nguyen.van.thuanb@sun-asterisk.com>
2020-06-19 09:14:00 +0700

    chapter 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_035448) do
+ActiveRecord::Schema.define(version: 2020_06_19_021242) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_035448) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
