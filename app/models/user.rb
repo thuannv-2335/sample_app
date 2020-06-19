326c98e3044fb54f937e0b11c7b25a241e1f8cf3
thuanbnv-1024 <nguyen.van.thuanb@sun-asterisk.com>
2020-06-19 10:09:57 +0700

    Chapter 9: Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = Settings.email_regex
   PERMIT_ATTRIBUTES = %i(name email password password_confirmation)
 
@@ -11,10 +13,17 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: Settings.user.password.min_length }
 
-  private
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
 
-  def downcase_email
-    email.downcase!
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
   end
 
   class << self
@@ -26,5 +35,16 @@ class User < ApplicationRecord
               end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
 end
